ca2ce58c43ac0f8ee0b5af351d2487fdd4cc8c7b
BAIDAR BAKHAT <mirbaidarmirbaidar@gmail.com>
2023-09-04 22:11:02 +0530

    push operation
    push operation in stack

--- stack.c
+++ stack.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
-int max = 0;
-void push(int arr[],int a){
-    max +=1;
-    arr[max]=a;
-}
-void pop(int arr[]){
-   max = max - 1; 
-  arr[max] = 0;
-}
-int main()
-{
-    int arr[10];
-    push(arr,11);
-    push(arr,12);
-    push(arr,13);
-    pop(arr);
-    
-    for(int i=0;i<= max;i++){
-        printf("position : arr[%d] = %d \n",i,arr[i]);
-    }
+int max=-1;
 
-    return 0;
+void push(int arr[],int enter){
+    max = max+1;
+      arr[max] = enter;
 }
+
+int main(){
+    int arr[10];
+    int a,addEelNo;
+    printf("enter the number of elements yoou wanna push in stack :");
+    scanf("%d",&addEelNo);
+
+    for(int add=0;add<addEelNo;add++){
+       printf("enter the element at index arr[%d] = ",add);
+       scanf("%d",&a);
+        push(arr,a);
+    }
+    
+
+//here the print of stack is performed
+int more = max;
+    for(int i=0;i<=more;i++){
+        printf("%d ",arr[i]);
+    }
+    return  0;
+}
+
