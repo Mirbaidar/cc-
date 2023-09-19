243acdb3d2d13bd088b118d84465c4d7b6a90875
BAIDAR BAKHAT <mirbaidarmirbaidar@gmail.com>
2023-09-19 21:41:39 +0530

    binary search algorithm
    binary search algorithm code written in c language to find the element in array in least time.

--- binarysearchInArray.c
+++ binarysearchInArray.c
@@ -7,38 +7,42 @@ int main()
     scanf("%d", &n);
     int arr[n];
 
-    printf("enter the sorted elements in array");
+    printf("enter the sorted elements in array : ");
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
     int lower_bond = 0;
     int upper_bond = n - 1;
+    
     int element;
     printf("enter the element to find :");
     scanf("%d",&element);
 
    int got =  binarySearch(arr, lower_bond, upper_bond, element);
-   printf("here the element location is %d",got);
+   printf("here the element location is arr[%d]",got);
     return 0;
 }
 
 int binarySearch(int arr[], int low, int high, int ele)
 {
-    int mid = low + high / 2;
+    int l_b = low;
+    int h_b = high;
 
-    while (arr[mid] != ele && low <= high)
+    int mid = (l_b + h_b) / 2;
+
+    while (arr[mid] != ele && low <= h_b)
     {
 
         if (arr[mid] < ele)
         {
-            low = mid + 1;
+            l_b = mid + 1;
         }
         else
         {
-            high = mid - 1;
+            h_b = mid - 1;
         }
-        mid = high + low / 2;
+        mid = (h_b + l_b )/ 2;
     }
     if (arr[mid] == ele)
     {
