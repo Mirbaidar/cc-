760d247c8b722371563faf0543dd55fc1e3a6b78
BAIDAR BAKHAT <mirbaidarmirbaidar@gmail.com>
2023-10-20 22:05:38 +0530

    Tail recursion
    Question 2  : - reverse  a number .

--- tailRecursion.c
+++ tailRecursion.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int factorial(int numb);
-//int reverse(int numb);
+int reverse(int numb);
 int main()
 {
     int num;
     printf("enter the number : \n");
     scanf("%d", &num);
     int fact = factorial(num);
-    printf("Number:%d , factorial:%d \n",num,fact);
+    printf("Number:%d , factorial:%d \n", num, fact);
+    int x = reverse(num);
+    printf("The reverse number is : %d \n", x);
 
     return 0;
 }
@@ -25,5 +27,22 @@ int factorial(int numb)
         f = f * numb;
         numb--;
         factorial(numb);
+    }
+}
+
+int reverse(int numb)
+{
+    static int rev = 0;
+    int dighit;
+    if (numb == 0)
+    {
+        return rev;
+    }
+    else
+    {
+        dighit = numb % 10;
+        rev = rev * 10 + dighit;
+        numb = numb / 10;
+        reverse(numb);
     }
 }
