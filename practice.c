7ee83cb78647099751faceea1ee30beb9c7ae727
BAIDAR BAKHAT <mirbaidarmirbaidar@gmail.com>
2024-01-08 20:10:13 +0530

    simple array questions

--- practice.c
+++ practice.c
@@ -1,42 +1,34 @@
-#include<stdio.h>
-#include<conio.h>
-int arr[10];
-int top =-1;
-char ch;
-void create();
-void display();
-int main(){
-   create();
-   display();
-    return 0;
-}
-
-void create(){
-      int n;
-      printf("Enter the element : \n");
-      scanf("%d",&n);
-      top=top+1;
-      arr[top]=n;
-     printf("want to continue .press Y or y : ");
-     ch=getche();
-     while(ch=='y' || ch=='Y'){
-      printf("Enter the element : \n");
-      scanf("%d",&n);
-      top=top+1;
-      arr[top]=n;
-      printf("want to continue .press Y or y : ");
-     ch=getche();
-     }
-}
-
-void display(){
-  if(top==-1){
-    printf("The stack is empty \n");
-  }else{
-    int i=top;
-    while(i>=0){
-      printf("%d  ",arr[i]);
-      i--;
-    }
+#include <stdio.h>
+int main()
+{
+  printf("\n");
+  int arr[5] = {10, 3, 6, 7, 67};
+  for (int i = 0; i < 5; i++)
+  {
+    printf("%d  ", arr[i]);
   }
+  printf("\n");
+  int j = 0;
+  int even_sum = 0, odd_sum = 0;
+  while (j < 5)
+  {
+    if (j == 0 || j % 2 == 0)
+    {
+      even_sum += arr[j];
+      arr[j] += 10;
+    }
+    else
+    {
+      odd_sum += arr[j];
+      arr[j] *= 2;
+    }
+    j++;
+  }
+  printf("\n");
+  for (int i = 0; i < 5; i++)
+  {
+    printf("%d  ", arr[i]);
+  }
+  printf("\nthe difference %d", even_sum - odd_sum);
+  return 0;
 }
